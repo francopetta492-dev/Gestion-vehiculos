dc08cddd59fe4b5c3623fd2e0f81df402d09fd11
Fede Petta <federicopetta234@gmail.com>
2026-06-10 14:33:30 -0300

    menu de opciones creados

---
+++ Gestion-vehiculos/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int op;
+        do
+        {
+            Console.WriteLine("--Menu--");
+            Console.WriteLine("1. Agregar auto");
+            Console.WriteLine("2. Agregar moto");
+            Console.WriteLine("3. Agregar camion");
+            Console.Write("Seleccione una opción: ");
+            op = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }while(op != 0);
+    }
+}
